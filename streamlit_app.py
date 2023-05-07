90e8e0767de1607cef695bc6f2a3734b9e8ac8f2
sam_volton <60001170+samvolton@users.noreply.github.com>
2023-05-07 12:24:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,6 @@ import pandas as pd
 import streamlit as st
 
 @st.cache
-def download_data(tickers):
-    # Download historical price data for each stock
-    prices = yf.download(tickers, start='2022-02-01', end='2023-05-05')['Adj Close']
-    return prices
-
 def calculate_z_scores(prices):
     # Calculate the rolling mean and standard deviation
     rolling_mean = prices.rolling(window=20).mean()
@@ -32,10 +27,10 @@ def identify_mean_reverting_stocks(tickers, z_scores):
     return mean_reverting_stocks
 
 # Define the ticker symbols of the preferred stocks you want to analyze
-tickers = ['AAPL', 'MSFT', 'GOOG', 'AMZN', 'META']
+tickers = ['AAPL', 'MSFT', 'GOOG', 'AMZN', 'FB']
 
 # Download historical price data for each stock
-prices = download_data(tickers)
+prices = yf.download(tickers, start='2022-02-01', end='2023-05-05')['Adj Close']
 
 # Calculate the z-score for each stock price
 z_scores = calculate_z_scores(prices)
@@ -45,15 +40,12 @@ mean_reverting_stocks = identify_mean_reverting_stocks(tickers, z_scores)
 
 # Create a Streamlit web app
 st.title('Mean-Reverting Stocks')
-st.write('Historical price data from 2022-02-01 to 2023-05-05')
-st.write(prices)
+st.write('Z-Scores from 2022-02-01 to 2023-05-05')
+st.write(z_scores)
 
 if len(mean_reverting_stocks) > 0:
     st.write('Mean-Reverting Stocks:')
     for stock, z_score in mean_reverting_stocks:
         st.write(f"{stock}: {z_score}")
-    st.write('Correlation Matrix:')
-    corr_matrix = z_scores.corr()
-    st.heatmap(corr_matrix, cmap='coolwarm')
 else:
     st.write('No mean-reverting stocks found.')
