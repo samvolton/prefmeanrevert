a37a428a6a55c984296dd184578770e2eca7cb5a
sam_volton <60001170+samvolton@users.noreply.github.com>
2023-05-07 21:56:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,7 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import numpy as np
-from datetime import datetime, timedelta
 
-
-# Function to calculate ATR
 def calculate_atr(high, low, close, n=14):
     hl = high - low
     hc = (high - close.shift()).abs()
@@ -15,78 +12,56 @@ def calculate_atr(high, low, close, n=14):
     atr = tr.ewm(alpha=1/n, adjust=False).mean()
     return atr
 
-
-# Function to download and process data
 @st.cache(suppress_st_warning=True, show_spinner=False)
-def get_stock_data(ticker, start_date, end_date):
+def get_stock_data(ticker, start_date, end_date, window):
     data = yf.download(ticker, start=start_date, end=end_date)
+    
+    if data.empty:
+        return None
 
     data['ATR'] = calculate_atr(data['High'], data['Low'], data['Close'], 14)
-    data['SMA'] = data['Close'].rolling(window=10).mean()
-    data['STD'] = data['Close'].rolling(window=10).std()
+    data['SMA'] = data['Close'].rolling(window=window).mean()
+    data['STD'] = data['Close'].rolling(window=window).std()
     data['Z_Score'] = (data['Close'] - data['SMA']) / data['STD']
-
+    
     return data.iloc[[-1]]
 
+tickers = ['MSFT', 'AAPL', 'META', 'GOOGL', 'AMD', 'AMZN']  # Replace with your 600 tickers
 
-def find_least_correlated_stocks(correlations, num_stocks):
-    selected_stocks = []
-    remaining_stocks = correlations.columns.tolist()
-
-    while len(selected_stocks) < num_stocks and len(remaining_stocks) > 0:
-        if not selected_stocks:
-            stock = remaining_stocks.pop(0)
-            selected_stocks.append(stock)
-        else:
-            min_corr = float('inf')
-            least_correlated_stock = None
-
-            for stock in remaining_stocks:
-                max_corr = correlations.loc[selected_stocks, stock].abs().max()
-                if max_corr < min_corr:
-                    min_corr = max_corr
-                    least_correlated_stock = stock
-
-            remaining_stocks.remove(least_correlated_stock)
-            selected_stocks.append(least_correlated_stock)
-
-    return selected_stocks
-
-
-# Preferred stock tickers
-tickers = ['MSFT', 'AAPL', 'META', 'GOOGL', 'AMD', 'AMZN']
-
-# Streamlit app
 st.title("Preferred Stocks Analysis")
 
 time_intervals = {
-    '1 Week': '7d',
-    '10 Days': '10d',
-    '1 Month': '1mo',
-    '1 Year': '1y',
-    '2 Years': '2y',
-    '5 Years': '5y',
+    '1 week': 7,
+    '1 month': 30,
+    '1 year': 365,
+    '2 years': 730,
+    '5 years': 1825
 }
 
-selected_interval = st.sidebar.selectbox('Time interval for correlations', list(time_intervals.keys()))
+selected_interval = st.sidebar.selectbox('Select time interval', list(time_intervals.keys()))
 
-end_date = datetime.today()
-start_date = end_date - pd.to_timedelta(time_intervals[selected_interval])
+end_date = "2023-05-07"
+start_date = pd.to_datetime(end_date) - pd.DateOffset(days=time_intervals[selected_interval])
+start_date = start_date.strftime("%Y-%m-%d")
 
-stock_data = pd.concat([get_stock_data(ticker, start_date, end_date).assign(Ticker=ticker) for ticker in tickers], ignore_index=True)
+window = max(1, time_intervals[selected_interval] // 3)
+
+with st.spinner('Loading stock data...'):
+    stock_data_list = [get_stock_data(ticker, start_date, end_date, window) for ticker in tickers]
+    stock_data_list = [data.assign(Ticker=ticker) for data, ticker in zip(stock_data_list, tickers) if data is not None]
+    stock_data = pd.concat(stock_data_list, ignore_index=True)
+
 stock_data = stock_data.set_index('Ticker')
 
-correlations = stock_data.T.pct_change().dropna().corr()
-
-num_stocks_in_portfolio = st.sidebar.slider('Number of stocks in portfolio', 1, len(tickers), 5)
-
-least_correlated_stocks = find_least_correlated_stocks(correlations, num_stocks_in_portfolio)
-portfolio = stock_data.loc[least_correlated_stocks]
+correlations = stock_data.pct_change().dropna().T.corr()
 
 st.header("Stock Parameters")
 st.write(stock_data[['ATR', 'SMA', 'STD', 'Z_Score']].T)
 
 st.header("Correlations")
-st.dataframe(correlations.style.highlight_max(axis=1))
+st.write("<style>table {width: 100% !important;}</style>", unsafe_allow_html=True)
+st.dataframe(correlations)
 
-st.header("Portfolio")
+st.header("Significant Z-Score Deviations")
+significant_deviation = stock_data[stock_data['Z_Score'].abs() > 1.5]
+st.write(significant_deviation[['ATR', 'SMA', 'STD', 'Z_Score']].T)
