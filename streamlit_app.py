c876fcf659f07b9f2a1e27e1b33f7aacaa9c15b7
sam_volton <60001170+samvolton@users.noreply.github.com>
2023-05-07 12:28:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
 import yfinance as yf
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import streamlit as st
 
 @st.cache
+def download_data(tickers):
+    # Download historical price data for each stock
+    prices = yf.download(tickers, start='2022-02-01', end='2023-05-05')['Adj Close']
+    return prices
+
 def calculate_z_scores(prices):
     # Calculate the rolling mean and standard deviation
     rolling_mean = prices.rolling(window=20).mean()
@@ -30,7 +36,7 @@ def identify_mean_reverting_stocks(tickers, z_scores):
 tickers = ['AAPL', 'MSFT', 'GOOG', 'AMZN', 'FB']
 
 # Download historical price data for each stock
-prices = yf.download(tickers, start='2022-02-01', end='2023-05-05')['Adj Close']
+prices = download_data(tickers)
 
 # Calculate the z-score for each stock price
 z_scores = calculate_z_scores(prices)
@@ -47,5 +53,7 @@ if len(mean_reverting_stocks) > 0:
     st.write('Mean-Reverting Stocks:')
     for stock, z_score in mean_reverting_stocks:
         st.write(f"{stock}: {z_score}")
+    st.write('Scatter Plot Matrix:')
+    sns.pairplot(z_scores)
 else:
     st.write('No mean-reverting stocks found.')
