679a97e7a38868e218521295f5ef0c0eb6995bfe
sam_volton <60001170+samvolton@users.noreply.github.com>
2023-05-07 12:37:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 @st.cache
 def download_data(tickers):
     # Download historical price data for each stock
-    prices = yf.download(tickers, start='2022-02-01', end='2023-05-05')['Adj Close']
+    prices = yf.download(tickers, start='2022-02-01', end='2023-05-06')['Adj Close']
     return prices
 
 @st.cache
