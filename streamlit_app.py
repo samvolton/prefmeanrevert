de9df718b4794f7c835506a489a99ef16c08b109
sam_volton <60001170+samvolton@users.noreply.github.com>
2023-05-07 14:40:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,52 +2,40 @@ import streamlit as st
 import yfinance as yf
 import pandas as pd
 import numpy as np
-import pandas_ta as ta
-import seaborn as sns
-import matplotlib.pyplot as plt
-from itertools import combinations
+import pyti.average_true_range as atr
 
 # Function to download and process data
+@st.cache(suppress_st_warning=True, show_spinner=False)
 def get_stock_data(ticker):
     start_date = "2020-01-01"
     end_date = "2023-05-07"
-
+    
     data = yf.download(ticker, start=start_date, end=end_date)
-
-    data['ATR'] = ta.average_true_range(data['High'], data['Low'], data['Close'])
+    
+    data['ATR'] = atr.average_true_range(data['High'], data['Low'], data['Close'], 14)
     data['SMA'] = data['Close'].rolling(window=10).mean()
     data['STD'] = data['Close'].rolling(window=10).std()
     data['Z_Score'] = (data['Close'] - data['SMA']) / data['STD']
-
+    
     return data.iloc[-1]
-
-# Function to calculate pairwise correlation
-def pairwise_correlation(data, tickers):
-    corr_data = data[tickers].pct_change().corr()
-    return corr_data
 
 # Preferred stock tickers
 tickers = ['AAPL', 'GOOGL', 'MSFT']  # Replace with the list of preferred stock tickers
 
-# Download and process data
-stock_data = pd.DataFrame()
-for ticker in tickers:
-    stock_data[ticker] = get_stock_data(ticker)
-
-# Calculate correlations
-correlations = pairwise_correlation(stock_data, tickers)
-
 # Streamlit app
 st.title("Preferred Stocks Analysis")
+
+with st.spinner('Loading stock data...'):
+    # Download and process data
+    stock_data = pd.DataFrame()
+    for i, ticker in enumerate(tickers):
+        stock_data[ticker] = get_stock_data(ticker)
+
+# Calculate correlations
+correlations = stock_data.T.pct_change().corr()
 
 st.header("Stock Parameters")
 st.write(stock_data[['ATR', 'SMA', 'STD', 'Z_Score']].T)
 
 st.header("Correlations")
 st.write(correlations)
-
-
-st.header("Correlation Heatmap")
-plt.figure(figsize=(10, 10))
-sns.heatmap(correlations, annot=True, cmap='coolwarm', linewidths=0.5, vmin=-1, vmax=1)
-st.pyplot(plt.gcf())
