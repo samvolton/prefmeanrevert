a5aac1ac76ec4f4d614edddd5bde81fdd3534da2
sam_volton <60001170+samvolton@users.noreply.github.com>
2023-05-07 15:00:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def get_stock_data(ticker):
     return data.iloc[[-1]]
 
 # Preferred stock tickers
-tickers = ['AAPL', 'GOOGL', 'MSFT']  # Replace with the list of preferred stock tickers
+tickers = ['BAC-PB', 'BAC-PE', 'BAC-PM', 'BAC-PN', 'BAC-PO', 'BAC-PP', 'BAC-PQ', 'BAC-PS', 'BANFP', 'BEP-PA', 'BEPH', 'BEPI', 'BFS-PD', 'BFS-PE', 'BHFAL', 'BHFAM', 'BHFAN', 'BHFAO', 'BHFAP', 'BHR-PD', 'BIP-PA', 'BIP-PB', 'BIPH', 'BIPI', 'BML-PG', 'BML-PH', 'BML-PJ', 'BML-PL', 'BNH', 'BNJ', 'BOH-PA', 'BPOPM', 'BPOPO', 'BPYPN', 'BPYPO', 'BPYPP', 'BW-PA', 'BWBBP', 'BWNB', 'BWSN', 'C-PJ', 'C-PK', 'C-PN']  # Replace with the list of preferred stock tickers
 
 # Streamlit app
 st.title("Preferred Stocks Analysis")
