1a112f4408fdf8c31f0cabc73a3526d7eb4921e2
sam_volton <60001170+samvolton@users.noreply.github.com>
2023-05-07 21:55:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,43 @@
 import streamlit as st
+import yfinance as yf
 import pandas as pd
 import numpy as np
-import yfinance as yf
-from datetime import datetime, timedelta
 
-def get_stock_data(ticker, start_date, end_date):
-    data = yf.download(ticker, start=start_date, end=end_date)
-    return data.iloc[[-1]]
 
-st.title('Mean Reverting Stocks')
+# Other functions are the same as before
 
-tickers_input = st.text_input('Enter your stock tickers (comma-separated)')
-tickers = [ticker.strip() for ticker in tickers_input.split(',') if ticker.strip()]
+def find_optimal_portfolio(correlations, min_stocks=2, max_stocks=None):
+    if max_stocks is None:
+        max_stocks = len(correlations.columns)
 
-start_date = (datetime.today() - timedelta(days=365 * 5)).strftime('%Y-%m-%d')
-end_date = datetime.today().strftime('%Y-%m-%d')
+    optimal_portfolio = []
+    min_avg_corr = float('inf')
 
-stock_data = pd.concat([get_stock_data(ticker, start_date, end_date).assign(Ticker=ticker) for ticker in tickers], ignore_index=True)
+    for num_stocks in range(min_stocks, max_stocks + 1):
+        least_correlated_stocks = find_least_correlated_stocks(correlations, num_stocks)
+        avg_corr = correlations.loc[least_correlated_stocks, least_correlated_stocks].mean().mean()
+        if avg_corr < min_avg_corr:
+            min_avg_corr = avg_corr
+            optimal_portfolio = least_correlated_stocks
 
-if not stock_data.empty:
-    stock_data = stock_data.set_index('Ticker')
-    st.write(stock_data)
+    return optimal_portfolio
 
-    st.subheader('Stock Correlations')
-    corr_periods = {
-        '1 Week': 5,
-        '10 Days': 10,
-        '1 Month': 21,
-        '3 Months': 63,
-        '6 Months': 126,
-        '1 Year': 252,
-        '2 Years': 504,
-        '5 Years': 1260
-    }
 
-    selected_period = st.selectbox('Select correlation period', list(corr_periods.keys()))
+# Streamlit app
+st.title("Preferred Stocks Analysis")
 
-    correlations = stock_data.corr().iloc[:,-1 * corr_periods[selected_period]:]
-    st.dataframe(correlations.style.highlight_max(axis=1).set_table_styles([{'selector': 'th', 'props': [('width', '100px')]}]))
+# The rest of the code remains the same
 
-    st.subheader('Portfolio Builder')
-    target_corr = st.slider('Target Correlation', -1.0, 1.0, -0.3, 0.1)
-    selected_stocks = correlations[(correlations < target_corr)].dropna(how='all').index.tolist()
-    st.write(f'Stocks with a correlation lower than {target_corr}: {", ".join(selected_stocks)}')
-else:
-    st.write('No data available for the entered tickers.')
+least_correlated_stocks = find_least_correlated_stocks(correlations, num_stocks_in_portfolio)
+portfolio = stock_data.loc[least_correlated_stocks]
+
+st.header("Stock Parameters")
+st.write(stock_data[['ATR', 'SMA', 'STD', 'Z_Score']].T)
+
+st.header("Correlations")
+st.dataframe(correlations.style.highlight_max(axis=1))
+
+st.header("Optimal Portfolio")
+optimal_portfolio = find_optimal_portfolio(correlations)
+optimal_portfolio_data = stock_data.loc[optimal_portfolio]
+st.write(optimal_portfolio_data[['ATR', 'SMA', 'STD', 'Z_Score']].T)
